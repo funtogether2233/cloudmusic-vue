92f21167f7b44b2316aa3bbae062fa5669cfd6ea
LiChen <2446432344@qq.com>
2022-09-05 22:33:08 +0800

    element

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,33 +8,48 @@
         />
         <p>网易云音乐</p>
       </div>
-      <div class="function-button">
-        <div class="switch"></div>
-        <div class="switch"></div>
-        <div class="search"></div>
-        <div class="listen"></div>
+      <div class="left-button">
+        <div class="switch">
+          <el-icon size="18px"><ArrowLeft /></el-icon>
+        </div>
+        <div class="switch">
+          <el-icon size="18px"><ArrowRight /></el-icon>
+        </div>
+        <div>
+          <el-input
+            v-model="searchInfo"
+            class="search"
+            placeholder="搜索"
+            prefix-icon="Search"
+          />
+        </div>
+        <div class="listen">
+          <el-icon size="22px"><Microphone /></el-icon>
+        </div>
       </div>
     </div>
     <div class="right">
       <div class="right-button">
-        <div class="switch"></div>
-        <div class="switch"></div>
-        <div class="switch"></div>
-        <div class="switch"></div>
-        <div class="switch"></div>
-        <div class="switch"></div>
-        <div class="switch"></div>
+        <el-icon size="22px"><Brush /></el-icon>
+        <el-icon size="22px"><Setting /></el-icon>
+        <el-icon size="22px"><Message /></el-icon>
+        <div>|</div>
+        <el-icon size="20px"><FullScreen /></el-icon>
+        <el-icon size="18px"><Minus /></el-icon>
+        <el-icon size="18px"><Close /></el-icon>
       </div>
       <div class="user">
         <img src="../assets/avatar.jpg" alt="cloudmusic-logo" />
         <p>网易云音乐</p>
-        <div class="switch"></div>
+        <el-icon size="12px"><CaretBottom /></el-icon>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
+const searchInfo = ref("");
 export default {};
 </script>
 
@@ -75,50 +90,78 @@ export default {};
   }
 }
 
-.function-button {
+.left-button {
   display: flex;
   align-items: center;
+  //   flex: 1;
 }
 
 .switch {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   margin: 0 5px;
   height: 30px;
   width: 30px;
-  background-color: white;
+  color: #e26a6a;
+  background-color: #d93b3b;
   border-radius: 50%;
+  :hover {
+    color: #f2baba;
+  }
 }
 
 .search {
-  margin: 0 10px;
-  height: 40px;
-  width: 200px;
-  background-color: white;
-  border-radius: 20px;
+  :deep(.el-input__wrapper) {
+    box-sizing: border-box;
+    margin: 0 10px;
+    padding: 0 16px;
+    height: 40px;
+    width: 200px;
+    color: #f2baba;
+    background-color: #e33e3e;
+    border-radius: 20px;
+    font-size: 18px;
+    box-shadow: 0 0 0 0;
+    ::placeholder {
+      color: #f2baba;
+    }
+    .el-input__prefix{
+      color: #f2baba;
+    }
+  }
 }
 
 .listen {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   height: 40px;
   width: 40px;
-  background-color: white;
+  color: #f2baba;
+  background-color: #e33e3e;
   border-radius: 50%;
 }
 
 .right-button {
   display: flex;
   justify-content: space-between;
+  align-items: center;
   margin-right: 25px;
   width: 300px;
+  color: #f2baba;
 }
 
 .user {
   display: flex;
   align-items: center;
+  margin: 0 10px;
   img {
     border-radius: 50%;
     height: 35px;
   }
   p {
-    margin-left: 6px;
+    margin: 0 6px;
     font-size: 16px;
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from "vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.use(ElementPlus, { size: "small", zIndex: 3000 });
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+app.mount("#app");
