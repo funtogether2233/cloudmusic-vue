600fd480a2c30f46f4f8ba09fa7568aa8f31e3c6
LiChen <2446432344@qq.com>
2022-09-25 19:29:51 +0800

    swiper

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,26 @@
       <li>歌手</li>
       <li>最新音乐</li>
     </ul>
-    <div class="carousel"></div>
+    <div class="carousel">
+      <swiper
+        :modules="modules"
+        :loop="true"
+        :slides-per-view="3"
+        :space-between="20"
+        :autoplay="{ delay: 4000, disableOnInteraction: false }"
+        navigation
+        :pagination="{ clickable: true }"
+        :scrollbar="{ draggable: true }"
+      >
+        <!-- loop可循环轮播，autoplay可自动播放 -->
+        <swiper-slide>Slide 1</swiper-slide>
+        <swiper-slide>Slide 2</swiper-slide>
+        <swiper-slide>Slide 3</swiper-slide>
+        <swiper-slide>Slide 4</swiper-slide>
+        <swiper-slide>Slide 5</swiper-slide>
+        <swiper-slide>Slide 6</swiper-slide>
+      </swiper>
+    </div>
     <div class="recommend">
       <div class="title">
         推荐歌单<el-icon size="16px"><ArrowRight /></el-icon>
@@ -19,8 +38,46 @@
 </template>
 
 <script>
+// 引入swiper组件
+import { Swiper, SwiperSlide } from "swiper/vue/swiper-vue.js";
+// 引入swiper样式（按需导入）
+// import "swiper/less";
+// import "swiper/less/pagination"; // 轮播图底面的小圆点
+// import "swiper/less/navigation"; // 轮播图两边的左右箭头
+// import "swiper/less/scrollbar"; // 轮播图的滚动条
+import "swiper/swiper.less";
+import "swiper/modules/pagination/pagination.less"; // 轮播图底面的小圆点
+import "swiper/modules/navigation/navigation.less"; // 轮播图两边的左右箭头
+import "swiper/modules/scrollbar/scrollbar.less"; // 轮播图的滚动条
+// import "swiper/swiper.min.css";
+// import "swiper/modules/pagination/pagination.min.css"; // 轮播图底面的小圆点
+// import "swiper/modules/navigation/navigation.min.css"; // 轮播图两边的左右箭头
+// import "swiper/modules/scrollbar/scrollbar.min.css"; // 轮播图的滚动条
+// 引入swiper核心和所需模块
+import { Autoplay, Pagination, Navigation, Scrollbar } from "swiper";
+// 在modules加入要使用的模块
+const modules = [Autoplay, Pagination, Navigation, Scrollbar];
+// const modules = [Ally, Pagination, Navigation, Scrollbar];
+
 export default {
   name: "Home",
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    const onSwiper = (swiper) => {
+      console.log(swiper);
+    };
+    const onSlideChange = () => {
+      console.log("slide change");
+    };
+    return {
+      onSwiper,
+      onSlideChange,
+      modules: [Autoplay, Pagination, Navigation, Scrollbar],
+    };
+  },
 };
 </script>
 
@@ -42,7 +99,6 @@ export default {
   overflow: auto;
   flex: 1;
   height: 100%;
-
   .nav {
     box-sizing: border-box;
     list-style: none;
