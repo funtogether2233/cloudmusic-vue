44d7ac51f58e1bdd2cbe210721535bb1e299ea87
LiChen <2446432344@qq.com>
2022-09-26 16:45:48 +0800

    playlist-box

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -34,16 +34,46 @@
           推荐歌单<el-icon size="16px"><ArrowRight /></el-icon>
         </div>
         <div class="playlist-content">
-          <div class="playlist-box"></div>
-          <div class="playlist-box"></div>
-          <div class="playlist-box"></div>
-          <div class="playlist-box"></div>
-          <div class="playlist-box"></div>
-          <div class="playlist-box"></div>
-          <div class="playlist-box"></div>
-          <div class="playlist-box"></div>
-          <div class="playlist-box"></div>
-          <div class="playlist-box"></div>
+          <div class="playlist-box">
+            <img src="../assets/cola-girl.png" />
+            <p>歌单1歌单1歌单1歌单1歌单1歌单1歌单1歌单1歌单1歌单1歌单1</p>
+          </div>
+          <div class="playlist-box">
+            <img src="../assets/cola-girl.png" />
+            <p>歌单2</p>
+          </div>
+          <div class="playlist-box">
+            <img src="../assets/cola-girl.png" />
+            <p>歌单3</p>
+          </div>
+          <div class="playlist-box">
+            <img src="../assets/cola-girl.png" />
+            <p>歌单4</p>
+          </div>
+          <div class="playlist-box">
+            <img src="../assets/cola-girl.png" />
+            <p>歌单5</p>
+          </div>
+          <div class="playlist-box">
+            <img src="../assets/cola-girl.png" />
+            <p>歌单6</p>
+          </div>
+          <div class="playlist-box">
+            <img src="../assets/cola-girl.png" />
+            <p>歌单7</p>
+          </div>
+          <div class="playlist-box">
+            <img src="../assets/cola-girl.png" />
+            <p>歌单8</p>
+          </div>
+          <div class="playlist-box">
+            <img src="../assets/cola-girl.png" />
+            <p>歌单9</p>
+          </div>
+          <div class="playlist-box">
+            <img src="../assets/cola-girl.png" />
+            <p>歌单10</p>
+          </div>
         </div>
       </div>
     </div>
@@ -161,7 +191,21 @@ export default {
           margin-bottom: 24px;
           height: 250px;
           width: 206px;
-          background-color: pink;
+        //   background-color: pink;
+          img {
+            border-radius: 5px;
+            height: 206px;
+            width: 206px;
+          }
+          p {
+            margin: 0;
+            color: #373737;
+          }
+          overflow: hidden;
+          text-overflow: ellipsis;
+          display: -webkit-box;
+          -webkit-box-orient: vertical;
+          -webkit-line-clamp: 3;
         }
       }
     }
