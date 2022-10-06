1d8baaf7a02d9a74550659dca019a746439700de
LiChen <2446432344@qq.com>
2022-10-06 17:17:49 +0800

    建立并引入axios实例

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,23 +1,25 @@
 <template>
   <el-carousel :interval="5000" type="card" height="198px">
     <el-carousel-item v-for="item in banners" :key="item">
-      <img :src="getImageUrl(item)" />
+      <!-- <img :src="getImageUrl(item)" /> -->
+      <img :src="item.imageUrl" />
     </el-carousel-item>
   </el-carousel>
 </template>
 
 <script>
+import http from "../plugins/http.js";
 export default {
   name: "Carousel",
   setup() {
-    const banners = [
-      "../assets/img/t2.jpg",
-      "../assets/img/t2.jpg",
-      "../assets/img/t2.jpg",
-      "../assets/img/t2.jpg",
-      "../assets/img/t2.jpg",
-      "../assets/img/t2.jpg",
-    ];
+    // const banners = [
+    //   "../assets/img/t2.jpg",
+    //   "../assets/img/t2.jpg",
+    //   "../assets/img/t2.jpg",
+    //   "../assets/img/t2.jpg",
+    //   "../assets/img/t2.jpg",
+    //   "../assets/img/t2.jpg",
+    // ];
     //   "@/assets/img/t2.jpg"
     const getImageUrl = (url) => {
       return new URL(`${url}`, import.meta.url).href;
@@ -28,6 +30,25 @@ export default {
     //     return banner.imageUrl;
     //   });
     // };
+    let banners;
+    http
+      .get("http://127.0.0.1:3000/banner")
+      .then(function (response) {
+        banners = response.data;
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    // const getBanner = () => {
+    //   $http({
+    //     url: "/banner",
+    //   }).then((res) => {
+    //     let { data } = res.data;
+    //     console.log(data);
+    //   });
+    // };
+    // getBanner();
     return {
       //   imgList,
       banners,
