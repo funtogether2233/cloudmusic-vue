a6bda50c69780a9808ec8d577ec2051f47a5416b
LiChen <2446432344@qq.com>
2022-10-06 15:57:30 +0800

    axios全局挂载

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from "vue";
 
+import axios from "axios";
+axios.defaults.baseURL = "http://127.0.0.1";
+
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
@@ -10,6 +13,7 @@ import "./assets/css/index.less";
 
 const app = createApp(App);
 
+app.config.globalProperties.$http = axios;
 app.use(ElementPlus, { size: "small", zIndex: 3000 });
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
