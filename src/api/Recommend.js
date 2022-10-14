a0573f83bcc445f019e45b0c54490d14790b2a57
LiChen <2446432344@qq.com>
2022-10-14 18:06:30 +0800

    增加getRecommendLists()函数

--- src/api/Recommend.js
+++ src/api/Recommend.js
@@ -1,22 +1,36 @@
-import http from "./request.js";
 import { getRequest } from "./request";
+import { translateCount } from "@/utils/util";
 
 const REQUEST_URL = {
   banner: "/banner",
+  recommendList: "/personalized",
 };
 // 获取轮播图
 export const getBanners = () => {
-  return (
-    // http
-    //   .get(REQUEST_URL.banner)
-      getRequest(REQUEST_URL.banner)
-      .then((response) => {
-        return response.data.banners.map((banner) => {
-          return banner.imageUrl;
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
-  );
+  return getRequest(REQUEST_URL.banner)
+    .then((res) => {
+      return res.data.banners.map((banner) => {
+        return banner.imageUrl;
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
+export const getRecommendLists = () => {
+  return getRequest(REQUEST_URL.recommendList, { limit: 10 })
+    .then((res) => {
+      return res.data.result.map((recommendList) => {
+        return {
+          id: recommendList.id,
+          name: recommendList.name,
+          picUrl: recommendList.picUrl,
+          playCount: translateCount(recommendList.playCount),
+        };
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 };
